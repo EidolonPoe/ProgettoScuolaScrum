5e8b0d392cb8a29ec07f0af49d50287a476a719c
FM <musorrafiti.f.2005@outlook.com>
2025-06-30 16:43:13 +0200

    Update ProgettoScrum

---
+++ ProgettoScrum/Classe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoScrum;
+
+public class Classe
+{
+    public int IdClasse { get; set; }
+    public int Anno { get; set; }
+    public string Sezione { get; set; }
+}
