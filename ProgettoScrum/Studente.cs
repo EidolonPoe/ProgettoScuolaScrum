8c6e099d64ca08a5435a4138f1b33a54df219580
Xavier Abril <xaiken99@gmail.com>
2025-06-30 16:52:38 +0200

    creazione classe studente

---
+++ ProgettoScrum/Studente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoScrum
+{
+    public class Studente
+    {
+        public string Matricola { get; set; }
+
+        public string Nome { get; set; }
+
+
+        public string Cognome { get; set; }
+
+        public int DataNascita { get; set; }
+
+        public  int  IdClasse { get; set; }
+
+    }
+}
