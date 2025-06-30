1061df88d7adad2673ebf13944894e8bf29c290a
FM <musorrafiti.f.2005@outlook.com>
2025-06-30 16:47:37 +0200

    Update ProgettoScrum

---
+++ ProgettoScrum/Voto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoScrum;
+
+public class Voto
+{
+    public int valore { get; set; }
+    public string materia { get; set; }
+}
