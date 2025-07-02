958b107cc84a4ac8f8a2e3047666a24d64d97bc8
FM <musorrafiti.f.2005@outlook.com>
2025-07-02 16:38:47 +0200

    Commit

--- ProgettoScrum/Repositories/Implementations/ClasseRepository.cs
+++ ProgettoScrum/Repositories/Implementations/ClasseRepository.cs
@@ -4,12 +4,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.Net.WebSockets;
 
 namespace ProgettoScrum.Repositories.Implementations
 {
     public class ClasseRepository : IClasseRepository
     {
         private readonly string ConnectionString;
+
+        public ClasseRepository(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public void Add(List<Studente> studenti)
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+
+                foreach (var studente in studenti)
+                {
+                    string query = @"
+                INSERT INTO Studenti (Nome, Cognome, IdClasse)
+                VALUES (@Nome, @Cognome, @IdClasse)";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Nome", studente.Nome);
+                        command.Parameters.AddWithValue("@Cognome", studente.Cognome);
+                        command.Parameters.AddWithValue("@IdClasse", studente.IdClasse);
+
+                        try
+                        {
+                            command.ExecuteNonQuery();
+                            Console.WriteLine($"Inserito: {studente.Nome} {studente.Cognome}");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Errore inserendo {studente.Nome} {studente.Cognome}: {ex.Message}");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
 
     }
 }
