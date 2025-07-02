a9e48c3bb92951eb0c3b128ecfc69fa3e9307d7c
Xavier Abril <xaiken99@gmail.com>
2025-07-02 16:30:16 +0200

    aggiunta interfaccia voto e materia

---
+++ ProgettoScrum/Repositories/Interfaces/IMateriaRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoScrum.Repositories.Interfaces
+{
+    public interface IMateriaRepository
+    {
+        void Add(Classe classe);
+        List<Classe> GetAll();
+        void Remove(int idClasse);
+        void Modify(Classe classe);
+    }
+}
---
+++ ProgettoScrum/Repositories/Interfaces/IVotoRepositorycs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoScrum.Repositories.Interfaces
+{
+    public interface IVotoRepositorycs
+    {
+
+        void Add(Voto voto);
+        List<Voto> GetAll();
+        void Remove(int idVoto);
+        void Modify(Voto voto);
+
+
+    }
+}
